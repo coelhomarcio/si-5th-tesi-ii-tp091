6d0a6358516e57a0c9e5a0c7483e4e9f446038de
Marcio Coelho <marciocoelho@gmail.com>
2021-10-06 10:33:12 -0300

    Updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { NgModule }                from "@angular/core";
-import { HttpClientModule }        from "@angular/common/http";
-import { FormsModule }             from "@angular/forms";
-import { MatButtonModule }         from "@angular/material/button";
-import { MatCardModule }           from "@angular/material/card";
-import { MatOptionModule }         from "@angular/material/core";
-import { MatFormFieldModule }      from "@angular/material/form-field";
-import { MatInputModule }          from "@angular/material/input";
-import { MatListModule }           from "@angular/material/list";
-import { MatMenuModule }           from "@angular/material/menu";
-import { MatPaginatorModule }      from "@angular/material/paginator";
-import { MatSelectModule }         from "@angular/material/select";
-import { MatTableModule }          from "@angular/material/table";
-import { MatTooltipModule }        from "@angular/material/tooltip";
 import { BrowserModule }           from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { registerLocaleData }      from "@angular/common";
-import br                          from "@angular/common/locales/pt";
+import { FormsModule }             from "@angular/forms";
+import { HttpClientModule }        from "@angular/common/http";
+
+import { registerLocaleData } from "@angular/common";
+import br                     from "@angular/common/locales/pt";
+
+import { AppRoutingModule }  from "./module/app-routing.module";
+import { AppMaterialModule } from "./module/app-material.module";
 
 import { AppComponent }            from "./app.component";
 import { CoelhoHeaderComponent }   from "./component/coelho-header/coelho-header.component";
@@ -24,10 +17,8 @@ import { CoelhoHomeComponent }     from "./component/coelho-home/coelho-home.com
 import { CoelhoCurrencyComponent } from "./component/coelho-currency/coelho-currency.component";
 import { CoelhoWalletComponent }   from "./component/coelho-wallet/coelho-wallet.component";
 
-import { AppRoutingModule }      from "./module/app-routing.module";
-import { AppMaterialModule }     from "./module/app-material.module";
-import { CoelhoIntervalService } from "./service/coelho-interval.service";
 import { CoelhoWalletService }   from "./service/coelho-wallet.service";
+import { CoelhoIntervalService } from "./service/coelho-interval.service";
 
 registerLocaleData(br);
 
@@ -40,24 +31,13 @@ registerLocaleData(br);
 		CoelhoCurrencyComponent,
 		CoelhoWalletComponent
 	],
-	imports: [
+	imports:      [
 		BrowserModule,
 		BrowserAnimationsModule,
+		FormsModule,
 		HttpClientModule,
 		AppRoutingModule,
-		AppMaterialModule,
-		MatListModule,
-		MatMenuModule,
-		MatButtonModule,
-		MatFormFieldModule,
-		FormsModule,
-		MatInputModule,
-		MatSelectModule,
-		MatOptionModule,
-		MatCardModule,
-		MatTableModule,
-		MatPaginatorModule,
-		MatTooltipModule
+		AppMaterialModule
 	],
 	providers:    [CoelhoWalletService, CoelhoIntervalService],
 	bootstrap:    [AppComponent]
