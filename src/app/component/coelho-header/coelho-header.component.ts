f34a6e8b846dcf52770db594d7064a08afc44a04
Marcio Coelho <marciocoelho@gmail.com>
2021-10-06 10:35:50 -0300

    Updated coelho-header.component.*

--- src/app/component/coelho-header/coelho-header.component.ts
+++ src/app/component/coelho-header/coelho-header.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from "@angular/core";
 
-import { Header }              from "../../interface/header";
+import { Header } from "../../interface/header";
+
 import { CoelhoWalletService } from "../../service/coelho-wallet.service";
 
 @Component({
