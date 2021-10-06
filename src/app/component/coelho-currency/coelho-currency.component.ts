d520b3c66bd79265b4185ec7f161f553e93d97ba
Marcio Coelho <marciocoelho@gmail.com>
2021-10-06 10:36:20 -0300

    Updated coelho-currency.component.*

--- src/app/component/coelho-currency/coelho-currency.component.ts
+++ src/app/component/coelho-currency/coelho-currency.component.ts
@@ -1,27 +1,22 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from "@angular/core";
-import { MatPaginator }                                from "@angular/material/paginator";
+import { Component, AfterViewInit, ViewChild } from "@angular/core";
+import { MatPaginator }                        from "@angular/material/paginator";
 
-import { CoelhoWalletService, Currency } from "../../service/coelho-wallet.service";
+import { CoelhoWalletService } from "../../service/coelho-wallet.service";
 
 @Component({
 	selector:    "app-coelho-currency",
 	templateUrl: "./coelho-currency.component.html"
 })
-export class CoelhoCurrencyComponent implements OnInit, AfterViewInit {
+export class CoelhoCurrencyComponent implements AfterViewInit {
 	public columns = ["timeISO", "USD.BRL", "EUR.BRL", "BTC.BRL"];
 
 	@ViewChild("paginator") paginator!: MatPaginator;
 
-	constructor(
-		public wallet: CoelhoWalletService
-	) {
+	constructor(public wallet: CoelhoWalletService) {
 	}
 
 	public tableData() {
 		return this.wallet.currencies;
-	}
-
-	ngOnInit(): void {
 	}
 
 	public ngAfterViewInit(): void {
