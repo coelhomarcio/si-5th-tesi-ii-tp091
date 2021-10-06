e86c0f16f05deac1becb383555d84a6076416f03
Marcio Coelho <marciocoelho@gmail.com>
2021-10-06 10:33:40 -0300

    Updated app-material.module.ts

--- src/app/module/app-material.module.ts
+++ src/app/module/app-material.module.ts
@@ -1,10 +1,26 @@
-import { NgModule }         from "@angular/core";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatIconModule }    from "@angular/material/icon";
+import { NgModule }           from "@angular/core";
+import { MatToolbarModule }   from "@angular/material/toolbar";
+import { MatButtonModule }    from "@angular/material/button";
+import { MatIconModule }      from "@angular/material/icon";
+import { MatMenuModule }      from "@angular/material/menu";
+import { MatListModule }      from "@angular/material/list";
+import { MatTableModule }     from "@angular/material/table";
+import { MatTooltipModule }   from "@angular/material/tooltip";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule }     from "@angular/material/input";
 
 const materials = [
 	MatToolbarModule,
-	MatIconModule
+	MatButtonModule,
+	MatIconModule,
+	MatMenuModule,
+	MatListModule,
+	MatTableModule,
+	MatTooltipModule,
+	MatPaginatorModule,
+	MatFormFieldModule,
+	MatInputModule
 ];
 
 @NgModule({
